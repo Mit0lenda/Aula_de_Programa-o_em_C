1a95d03fc381df3a8152e028bc3a66644f499d8c
Nicollas De Oliveira <nideoliveirafreitas@gmail.com>
2023-05-18 23:41:40 -0300

    att

--- Aula_de_matriz/Exercício_6.c
+++ Aula_de_matriz/Exercício_6.c
@@ -1,7 +1,7 @@
 //Faça um programa que crie duas matrizes 4 x 4 de inteiros  e posteriormente cria  uma terceira com os maiores valores de cada posição das matrizes lidas.
 #include <stdio.h>
 int main(){
-  int matriz[4][4],l,c,matriz1[4][4],matriz2;
+  int matriz[4][4],l,c,matriz1[4][4],matriz2[4][4];
   for (l=0;l<4;l++)
   {
     for (c=0;c<4;c++)
@@ -27,11 +27,13 @@ int main(){
     {
       if (matriz[l][c]>matriz1[l][c])
       {
-         printf("%d",matriz[l][c]);
+        matriz2[l][c]=matriz[l][c];
+        printf("%d",matriz2[l][c]);
       }
       else
       {
-         printf("%d",matriz1[l][c]);
+        matriz2[l][c]=matriz1[l][c];
+        printf("%d",matriz2[l][c]);
       }
 
     }
